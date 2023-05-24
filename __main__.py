189bb7025d0cb3cae2c3980a67e600bc6a194377
LaughableHammer <102272266+kushaagrak@users.noreply.github.com>
2023-05-25 08:46:50 +1000

    Using Main.py as main file now

--- __main__.py
+++ __main__.py
@@ -1,3 +1,13 @@
-from PassfinderLogic.passfinder import main
+from UserInterface import UI, LoginUI2 as LoginUI
 
-main()
+def main():
+    print("Hello, World!")
+
+    app = UI.PassFinder()
+
+    LoginUI.loginUI(app)
+
+    app.root.mainloop()
+
+if __name__ == '__main__':
+    main()
